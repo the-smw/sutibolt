8eb9fe00e97bcbc380992af824bcc27e1b6a0560
the-smw <62156804+the-smw@users.noreply.github.com>
2021-01-05 16:06:35 +0100

    added cookie consent bar and favicon

--- script.js
+++ script.js
@@ -79,3 +79,20 @@ scrollToTopButton.onclick = function(e) {
 
 
 
+
+// Cookie container
+
+$(document).ready(function(){   
+  if (window.localStorage.getItem('accept_cookies')) {
+        $(".cookie-container").hide();
+    }
+  
+    $(".cookie-btn").click(function() {
+   window.localStorage.setItem('accept_cookies', true);
+        $(".cookie-container").fadeOut(1000);
+    }); 
+}); 
+
+
+
+
